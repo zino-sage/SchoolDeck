6010e73f561337b2d9ff0436294eee7af605eb40
mazino <chisonm.okafor@fintraksoftware.com>
2024-08-03 10:53:16 +0100

    project setup

--- SchoolDeck/Program.cs
+++ SchoolDeck/Program.cs
@@ -1,14 +1,35 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using SchoolDeck.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration(); 
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
